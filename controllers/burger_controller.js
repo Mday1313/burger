2aae1c6cb0b8ac9e685ec3f90e60ec54af9f1a2f
Mday1313 <melissaday076@gmail.com>
2019-05-27 23:28:03 -0400

    Devour button not functioning, style css

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -29,6 +29,22 @@ router.post("/api/burgers", function(req, res) {
   });
 });
 
+router.put("/api/burgers/:id", function(req,res){
+  var condition = "id = " + req.params.id;
+  
+  console.log("condition", condition + " devour status "+ req.body.devoured);
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0){
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+    
+  });
+});
+
 
 
 
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,22 @@
 // on click to create burger
 $(function () {
+    $(".change-status").on("click", function (event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newstatus");
+console.log(newDevour);
+        var newDevourStatus = {
+            devoured: newDevour
+        };
 
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourStatus
+        }).then(
+            function () {
+                location.reload();
+            }
+        );
+    });
 
 
     $(".create-form").on("submit", function (event) {
